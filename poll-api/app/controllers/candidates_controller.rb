47bdeb804c93b9c6b511fb0145257f68a1a4382c
Eunbyul Cho <hello@eunbyulcho.me>
2019-07-05 00:53:01 +0900

    add action to candidate controller

--- poll-api/app/controllers/candidates_controller.rb
+++ poll-api/app/controllers/candidates_controller.rb
@@ -1,2 +1,45 @@
 class CandidatesController < ApplicationController
+  before_action :set_poll
+  before_action :set_poll_candidate, only: [:show, :update, :destroy]
+
+  # GET /polls/:poll_id/candidates
+  def index
+    json_response(@poll.candidates)
+  end
+
+  # GET /polls/:poll_id/candidates/:id
+  def show
+    json_response(@candidate)
+  end
+
+  # POST /polls/:poll_id/candidates
+  def create
+    @poll.candidates.create!(candidate_params)
+    json_response(@poll)
+  end
+  # PUT /polls/:poll_id/candidates/:id
+  def update
+    @candidate.update(candidate_params)
+    head :no_content
+  end
+
+  # DELETE /polls/:poll_id/candidates/:id
+  def destroy
+    @candidate.destroy
+    head :no_content
+  end
+
+  private
+
+  def candidate_params
+    params.permit(:name, :count)
+  end
+
+  def set_poll
+    @poll = Poll.find(params[:poll_id])
+  end
+
+  def set_poll_candidate
+    @candidate = @poll.candidates.find_by!(id: params[:id]) if @poll
+  end
 end
