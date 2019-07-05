9516517af827fe51a5c8c2a629f31f5999d67a25
Eunbyul Cho <hello@eunbyulcho.me>
2019-07-05 10:37:29 +0900

    use as_json to customize result

--- poll-api/app/models/candidate.rb
+++ poll-api/app/models/candidate.rb
@@ -1,4 +1,7 @@
 class Candidate < ApplicationRecord
   belongs_to :poll
   validates_presence_of :name
+  def as_json(options = nil)
+    super({ only: [:id, :name, :count] }.merge(options || {}))
+  end
 end
--- poll-api/app/models/poll.rb
+++ poll-api/app/models/poll.rb
@@ -1,4 +1,7 @@
 class Poll < ApplicationRecord
   has_many :candidates, dependent: :destroy
   validates_presence_of :name
+  def as_json(options = nil)
+    super({ only: [:id, :name] }.merge(options || {}))
+  end
 end
