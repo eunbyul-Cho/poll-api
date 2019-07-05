3575701a77cd8be1f67ef19d8fa9387cba66b9de
Eunbyul Cho <hello@eunbyulcho.me>
2019-07-05 14:12:44 +0900

    add user controller

--- poll-api/config/routes.rb
+++ poll-api/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :polls do
     resources :candidates
   end
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
 end
