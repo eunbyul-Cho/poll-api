572d80a031ac903b662d312c41a7a177f43990ef
Eunbyul Cho <hello@eunbyulcho.me>
2019-07-12 18:02:48 +0900

    make action folder, reducers

--- poll-app/src/actions/index.js
+++ poll-app/src/actions/index.js
@@ -1,7 +1,39 @@
 import api from "../lib/api.js";
 
-export const GET_POLLS = "GET_POLLS";
+export function itemsHasErrored(bool) {
+  return {
+    type: "ITEMS_HAS_ERRORED",
+    hasErrored: bool
+  };
+}
+export function itemsIsLoading(bool) {
+  return {
+    type: "ITEMS_IS_LOADING",
+    isLoading: bool
+  };
+}
+export function itemsFetchDataSuccess(items) {
+  return {
+    type: "ITEMS_FETCH_DATA_SUCCESS",
+    items
+  };
+}
+export function itemsFetchData(url) {
+  return dispatch => {
+    dispatch(itemsIsLoading(true));
 
-export const getPolls = () => ({
-  type: GET_POLLS
-});
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+
+        dispatch(itemsIsLoading(false));
+
+        return response;
+      })
+      .then(response => response.json())
+      .then(items => dispatch(itemsFetchDataSuccess(items)))
+      .catch(() => dispatch(itemsHasErrored(true)));
+  };
+}
