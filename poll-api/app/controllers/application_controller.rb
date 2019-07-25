8aa0b308e0b2f18023dada7fe4e80eeace0ad973
Eunbyul Cho <hello@eunbyulcho.me>
2019-07-25 12:37:41 +0900

    use cors in config/application

--- poll-api/app/controllers/application_controller.rb
+++ poll-api/app/controllers/application_controller.rb
@@ -7,11 +7,7 @@ class ApplicationController < ActionController::API
   before_action :authorize_request
   attr_reader :current_user
   private
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :options]
-  end
+
     # Check for valid request token and return user
   def authorize_request
     @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
